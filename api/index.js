4385cf2785912cd19c59397d1811c14f87fe7a70
iprefersun <jaym0625@gmail.com>
2026-03-28 17:45:46 -0700

    Fix odds API handler and improve error handling

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res) {
   const API_KEY = process.env.ODDS_API_KEY;
   const sport = req.query.sport || 'basketball_nba';
-
   const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us&markets=h2h,spreads&oddsFormat=american&dateFormat=iso`;
-
   try {
     const response = await fetch(url);
     const data = await response.json();
