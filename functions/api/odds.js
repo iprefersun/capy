5a67caeee2c1a86012b342cee7b2014209e70a69
iprefersun <jaym0625@gmail.com>
2026-03-28 17:17:42 -0700

    Remove odds API handler function
    Removed the odds API handler function from odds.js.

--- functions/api/odds.js
+++ functions/api/odds.js
@@ -1,15 +1,1 @@
-export default async function handler(req, res) {
-  const API_KEY = process.env.ODDS_API_KEY;
-  const sport = req.query.sport || 'basketball_nba';
 
-  const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us&markets=h2h,spreads&oddsFormat=american&dateFormat=iso`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch odds' });
-  }
-}
